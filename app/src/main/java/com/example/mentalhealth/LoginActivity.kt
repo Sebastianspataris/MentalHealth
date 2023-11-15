3ed850b81c129e1ffd1bb476e4bcc863c380a57f
Jemblish <ianbouey1@gmail.com>
2023-11-14 21:46:43 -0300

    Registro aplicado pero no se muestra la pestaña de diagnostico.

--- app/src/main/java/com/example/mentalhealth/LoginActivity.kt
+++ app/src/main/java/com/example/mentalhealth/LoginActivity.kt
@@ -6,11 +6,14 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
+
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var editTextUsername: EditText
     private lateinit var editTextPassword: EditText
     private lateinit var btnLogin: Button
+    private lateinit var btnGoToRegister: Button // Boton registro
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,24 @@ class LoginActivity : AppCompatActivity() {
         editTextUsername = findViewById(R.id.editTextUsername)
         editTextPassword = findViewById(R.id.editTextPassword)
         btnLogin = findViewById(R.id.btnLogin)
+        btnGoToRegister = findViewById(R.id.btnGoToRegister)
+
+        btnGoToRegister.setOnClickListener {
+            // Cuando se hace clic en el botón de registro, abre la actividad de registro
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+
+
+        btnLogin.setOnClickListener {
+            // Tu lógica de inicio de sesión aquí...
+
+            // Después de realizar el inicio de sesión, puedes abrir la MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
 
         btnLogin.setOnClickListener {
             val enteredUsername = editTextUsername.text.toString()
@@ -34,4 +55,5 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
+}
 }
---
+++ app/src/main/java/com/example/mentalhealth/RegistroActivity.kt
@@ -0,0 +1,51 @@
+package com.example.mentalhealth
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mentalhealth.R
+
+class RegistroActivity : AppCompatActivity() {
+
+    private lateinit var firstNameEditText: EditText
+    private lateinit var lastNameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var dobEditText: EditText
+    private lateinit var diagnosisSpinner: Spinner
+    private lateinit var registerButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registro_activity)
+
+        // Inicializar vistas
+        firstNameEditText = findViewById(R.id.editTextFirstName)
+        lastNameEditText = findViewById(R.id.editTextLastName)
+        emailEditText = findViewById(R.id.editTextEmail)
+        dobEditText = findViewById(R.id.editTextDOB)
+        diagnosisSpinner = findViewById(R.id.spinnerDiagnosis)
+        registerButton = findViewById(R.id.buttonRegister)
+
+        // Configurar el clic del botón de registro
+        registerButton.setOnClickListener {
+            realizarRegistro()
+        }
+    }
+
+    private fun realizarRegistro() {
+        // Aquí puedes obtener los valores de los EditText y Spinner
+        val firstName = firstNameEditText.text.toString()
+        val lastName = lastNameEditText.text.toString()
+        val email = emailEditText.text.toString()
+        val dob = dobEditText.text.toString()
+        val diagnosis = diagnosisSpinner.selectedItem.toString()
+
+        // Puedes realizar la lógica de registro aquí
+        // Por ejemplo, podrías validar los datos y luego mostrar un mensaje Toast
+        val mensaje = "Registro exitoso:\nNombre: $firstName $lastName\nCorreo: $email\nDOB: $dob\nDiagnóstico: $diagnosis"
+        Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+    }
+}
