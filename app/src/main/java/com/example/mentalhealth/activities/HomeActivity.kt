470f488e5c7e909f619cb26947d2a5cd94ce0644
Sebastianspataris <146017702+Sebastianspataris@users.noreply.github.com>
2023-11-19 23:17:55 -0300

    create bottom navigation on home

--- app/src/main/java/com/example/mentalhealth/activities/HomeActivity.kt
+++ app/src/main/java/com/example/mentalhealth/activities/HomeActivity.kt
@@ -1,17 +1,37 @@
 package com.example.mentalhealth.activities
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mentalhealth.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var bottomNavigation: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
     }
 
     override fun onStart() {
         super.onStart()
-
+        bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.page_1 -> {
+                    //Aca cuando seleccionas Monitorar
+                    Toast.makeText(this, "Vamos a monitorear!", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.page_2 -> {
+                    //Aca cuando seleccionas Notas
+                    Toast.makeText(this, "Vamos a Ir a Notas!", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
