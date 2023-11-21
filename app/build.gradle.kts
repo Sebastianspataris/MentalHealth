5ed24e28cf016e8fa2fc62b71b7bfda6cf0dfd84
nicolas gajardo <gajardonicolas358@gmail.com>
2023-11-20 21:05:52 -0300

    conexion con base de datos, falta configurar base de datos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.google.firebase:firebase-database:23.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
