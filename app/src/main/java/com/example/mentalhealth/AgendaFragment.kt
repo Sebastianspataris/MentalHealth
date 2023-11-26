3d23c9442ba3b80197da1d10674ae6628341b418
Jemblish <ianbouey1@gmail.com>
2023-11-25 22:19:17 -0300

    Se crea el evento
    se agrego un boton para crear el evento y un cuadro de texto para ponerle nombre

--- app/src/main/java/com/example/mentalhealth/AgendaFragment.kt
+++ app/src/main/java/com/example/mentalhealth/AgendaFragment.kt
@@ -1,45 +1,44 @@
 package com.example.mentalhealth
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CalendarView
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AgendaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AgendaFragment : Fragment() {
 
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var selectedDate: Calendar
+    private lateinit var eventNameEditText: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_agenda, container, false)
 
         val calendarView: CalendarView = view.findViewById(R.id.calendarView)
+        val createEventButton: Button = view.findViewById(R.id.createEventButton)
+        eventNameEditText = view.findViewById(R.id.eventNameEditText)
+
         calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
-            val selectedDate = "$dayOfMonth/${month + 1}/$year"
-            showToast("Fecha seleccionada: $selectedDate")
+            selectedDate = Calendar.getInstance().apply {
+                set(year, month, dayOfMonth)
+            }
+            showToast("Fecha seleccionada: ${dayOfMonth}/${month + 1}/${year}")
+        }
+
+        createEventButton.setOnClickListener {
+            // Lógica para crear un evento simple con solo la fecha y el nombre del evento
+            createSimpleEvent(selectedDate, eventNameEditText.text.toString())
         }
 
         return view
@@ -49,14 +48,12 @@ class AgendaFragment : Fragment() {
         Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AgendaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun createSimpleEvent(eventDate: Calendar, eventName: String) {
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        val formattedDate = dateFormat.format(eventDate.time)
+
+        val eventDetails = "Evento: $eventName\nFecha: $formattedDate"
+
+        showToast(eventDetails)
     }
 }
